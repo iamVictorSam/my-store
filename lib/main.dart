32be44141a76465a583ce02b62777bf12ad55241
IamVictorSam <iamvictorsam@gmail.com>
2022-07-21 15:03:31 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Flutter Demo',
+      title: 'Medusa Store',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
